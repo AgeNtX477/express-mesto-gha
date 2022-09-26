8cddde41873901c3a4b4746d5906b0620608bdcb
Jury Obuhov <jury2500@gmail.com>
2022-09-26 16:16:44 +0300

    project 13th done-12

--- controllers/users.js
+++ controllers/users.js
@@ -21,12 +21,12 @@ module.exports.getUsers = (req, res) => {
 };
 
 module.exports.getUserId = (req, res) => {
-  User.findById(req.params.id)
+  User.findById(req.params.userId)
     .then((data) => {
       if (!data) {
-        res.status(ERROR_404).send({ message: 'Пользователь по указанному _id не найден.' });
+        return res.status(ERROR_404).send({ message: 'Пользователь по указанному _id не найден.' });
       }
-      res.send({ data });
+      return res.send(data);
     })
     .catch((err) => res.status(ERROR_500).send({ message: err.message }));
 };
