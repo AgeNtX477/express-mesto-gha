36c75a25e1b6b918ca0ff89aff9666cf3166fe05
Jury Obuhov <jury2500@gmail.com>
2022-09-26 14:49:26 +0300

    project 13th done-3

--- app.js
+++ app.js
@@ -7,7 +7,7 @@ const users = require('./routes/users');
 const { PORT = 3000 } = process.env;
 
 const app = express();
-mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
+mongoose.connect('mongodb://localhost:27017/mestodb');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -20,6 +20,9 @@ app.use((req, res, next) => {
 
 app.use('/', users);
 app.use('/', cards);
+app.use('*', (req, res) => {
+  res.status(404).send({ message: 'Страница не найдена' });
+});
 
 app.listen(PORT, () => {
   console.log(`App listening on port ${PORT}...`);
