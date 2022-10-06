e4d7217076f2bdcd4d3f6e3ff4afa667391fcb8e
Jury Obuhov <jury2500@gmail.com>
2022-10-06 23:33:35 +0300

    trying to fix get-users errors-28

--- routes/users.js
+++ routes/users.js
@@ -8,23 +8,27 @@ const {
   updateAvatar,
 } = require('../controllers/users');
 
-router.get('/users', getUsers);
+router.get('/users/me', getCurrentUser);
+
 router.get('/users/:userId', celebrate({
   params: Joi.object().keys({
     userId: Joi.string().length(24).hex().required(),
   }),
 }), getUserId);
-router.get('/users/me', getCurrentUser);
+
 router.patch('/users/me', celebrate({
   body: Joi.object().keys({
     name: Joi.string().min(2).max(30),
     about: Joi.string().min(2).max(30),
   }),
 }), updateUser);
+
 router.patch('/users/me/avatar', celebrate({
   body: Joi.object().keys({
     avatar: Joi.string().pattern(/(https?:\/\/)(w{3}\.)?([a-zA-Z0-9-]{0,63}\.)([a-zA-Z]{2,4})(\/[\w\-._~:/?#[\]@!$&'()*+,;=]#?)?/),
   }),
 }), updateAvatar);
+
+router.get('/users', getUsers);
 
 module.exports = router;
