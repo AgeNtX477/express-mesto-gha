21dd5f684e28146e6bfcb7f9f646046495ec25ef
Jury Obuhov <jury2500@gmail.com>
2022-10-07 00:38:53 +0300

    added card-controllers errors-2

--- controllers/cards.js
+++ controllers/cards.js
@@ -33,7 +33,7 @@ module.exports.deleteCard = (req, res, next) => {
         throw new NotFoundErr('Карточка с указанным _id не найдена.');
       }
       if (!card.owner.equals(req.user._id)) {
-        return next(new ForbiddenErr('Нельзя удалить карточку другого пользователя.'));
+        throw new ForbiddenErr('Нельзя удалить карточку другого пользователя.');
       }
       return res.status(200).send({ data: card });
     })
