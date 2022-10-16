dc6c0b1ee4c52f9e0904036a8e19fef0ae56e76b
Jury Obuhov <jury2500@gmail.com>
2022-10-16 16:31:15 +0300

    added: CORS npm, upadete CORS code-3

--- app.js
+++ app.js
@@ -67,6 +67,41 @@ app.use(cors({
   credentials: true,
 }));
 
+const allowedCors = [
+  'https://praktikum.tk/',
+  'http://praktikum.tk/',
+  'https://localhost:3000/',
+  'http://localhost:3000/',
+  'localhost:3000',
+  'https://127.0.0.1:3000/',
+  'http://127.0.0.1:3000/',
+  '127.0.0.1:3000',
+  'https://127.0.0.1:27017/',
+  'http://127.0.0.1:27017/',
+  '127.0.0.1:27017',
+];
+
+app.use((req, res, next) => {
+  const { origin } = req.headers;
+  if (allowedCors.includes(origin)) {
+    res.header('Access-Control-Allow-Origin', origin);
+  }
+  next();
+});
+
+// eslint-disable-next-line consistent-return
+app.use((req, res, next) => {
+  const { method } = req;
+  const requestHeaders = req.headers['access-control-request-headers'];
+  const DEFAULT_ALLOWED_METHODS = 'GET,HEAD,PUT,PATCH,POST,DELETE';
+  if (method === 'OPTIONS') {
+    res.header('Access-Control-Allow-Methods', DEFAULT_ALLOWED_METHODS);
+    res.header('Access-Control-Allow-Headers', requestHeaders);
+    return res.end();
+  }
+  next();
+});
+
 app.use(errorLogger);
 app.use(errors());
 app.use((err, req, res, next) => {
