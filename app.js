7648ca44c69d1db18809ad9df05c0860f9463b25
Jury Obuhov <jury2500@gmail.com>
2022-10-17 00:01:50 +0300

    added: credentials

--- app.js
+++ app.js
@@ -15,7 +15,9 @@ const { PORT = 3000 } = process.env;
 
 const app = express();
 mongoose.connect('mongodb://127.0.0.1:27017/mestodb');
-app.use(cors());
+app.use(cors({
+  credentials: true,
+}));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(requestLogger);
